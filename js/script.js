24db6836edae87df6b6d92049220f0d8a7766f5b
Erica <ericasrafael@gmail.com>
2021-10-31 12:55:07 -0300

    :package: novas funcionalidades

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+
+document.querySelector("#quantidade").addEventListener("change", atualizarPreco)
+function atualizarPreco(){
+    const quantidade = document.querySelector("#quantidade").value
+    const temProjeto = document.querySelector("#projeto").checked
+    let preco = quantidade * 200;
+    
+    if(quantidade => 10) preco *= 0.75;
+    else preco = quantidade * 200;
+
+    document.querySelector("#preco").innerHTML = "R$" + preco.toFixed(2)
+}
+
